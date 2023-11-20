09017d570d289eeeee51efd9a5a811861e5b3f73
fortune-0 <fortunepeterspc07@gmail.com>
2023-11-20 04:53:24 -0800

    linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "lists.h"
+/** frees a linked list
+ */
+
+void free_list(list_t *head)
+{
+list_t *tmp;
+while (head)
+{
+tmp = head->next;
+free(head->str);
+free(head);
+head = tmp;
+}
+}
+
